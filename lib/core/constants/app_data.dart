f755c402fe8b6024dd71de2d23b760f981f3ae7f
Ahmed Hosni <96553177+ahmedhosni803@users.noreply.github.com>
2026-01-18 11:36:24 +0200

    Update app_data.dart

--- lib/core/constants/app_data.dart
+++ lib/core/constants/app_data.dart
@@ -61,22 +61,37 @@ static final List<Project> projects = [
     description:
         'Production-grade ecosystem including customer, shops, restaurants, and delivery applications.',
     imageUrl:
-        'https://play-lh.googleusercontent.com/6G8u9nY5D3y7QYc6Kx8n3U4k5lR2p9JxZK=s180',
+        'https://play-lh.googleusercontent.com/efFcBNqFehYRNd-io13vKMUz1sD7I8huT5QT6fNoG2u_CVdZVS0GwdfZCFseRQMdbQTg=w600-h300-pc0xffffff-pd',
     links: [
       const ProjectLink(
         title: 'Customer App',
         googlePlay:
             'https://play.google.com/store/apps/details?id=com.extreme.help',
+      
+        appleStore:
+            'https://apps.apple.com/eg/app/findhelpapp/id1450736684',
       ),
       const ProjectLink(
         title: 'Shops App',
         googlePlay:
-            'https://play.google.com/store/apps/details?id=com.extreme.shopprovider',
+            'https://play.google.com/store/apps/details?id=com.extreme.shopprovider'
+        ,
+             appleStore:
+            'https://apps.apple.com/eg/app/findhelp-shop-owners/id6475237145',
+      ),
+        const ProjectLink(
+        title: 'Restaurant App',
+        googlePlay:
+            'https://play.google.com/store/apps/details?id=com.extreme.provider',
+               appleStore:
+            'https://apps.apple.com/eg/app/findhelp-restaurant-owners/id6451415009',
       ),
       const ProjectLink(
         title: 'Delivery App',
         googlePlay:
-            'https://play.google.com/store/apps/details?id=com.extreme.delivery',
+            'https://play.google.com/store/apps/details?id=com.extreme.find_help_delivery_app',
+             appleStore:
+            'https://apps.apple.com/eg/app/findhelp-delivery/id6473175356',
       ),
     ],
   ),
@@ -87,14 +102,28 @@ static final List<Project> projects = [
     description:
         'Meat shopping application offering a fast and convenient shopping experience.',
     imageUrl:
-        'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/8c/1e/2b/8c1e2b4d-3c9b-4a2f-9b3d-6e7b5d1f4a2c/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/1200x630wa.png',
+        'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/97/89/4e/97894e36-8024-5d9b-7b84-c0c9cda6a863/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/1200x630wa.png',
     links: [
       const ProjectLink(
-        title: 'App',
+        title: 'User App',
         googlePlay:
-            'https://play.google.com/store/apps/details?id=com.meatoo.app',
+            'https://play.google.com/store/apps/details?id=net.meatoo.user',
         appleStore:
-            'https://apps.apple.com/eg/app/meatoo/id6448787340',
+            'https://apps.apple.com/sa/app/meatoo/id6741719725',
+      ),
+      const ProjectLink(
+        title: 'Meatoo Vendor',
+        googlePlay:
+            'https://play.google.com/store/apps/details?id=net.meatoo.vendor',
+        appleStore:
+            'https://apps.apple.com/sa/app/meatoo-vendor/id6742039572',
+      ),
+           const ProjectLink(
+        title: 'Meatoo delivery',
+        googlePlay:
+            'https://play.google.com/store/apps/details?id=net.meatoo.delivery',
+        appleStore:
+            'https://apps.apple.com/sa/app/meatoo-delivery/id6741743993',
       ),
     ],
   ),
@@ -104,8 +133,8 @@ static final List<Project> projects = [
     subtitle: 'On-Demand Services',
     description:
         'Saudi application for water filter maintenance and technician services with online payments.',
-    imageUrl:
-        'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/93/ca/df/93cadf87-0aa6-4a68-0db9-f8a088c60f69/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/1200x630wa.png',
+    imageUrl:"https://play-lh.googleusercontent.com/U11n5Hm-JmCmx4hgynP4mPOL62wCX6E7z8c3imZLNPMVi7ycIAEeBzRwCpf06uJHlU17vVTNFS5UG-r9Pcwm-g=w526-h296-rw",
+        // 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/93/ca/df/93cadf87-0aa6-4a68-0db9-f8a088c60f69/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/1200x630wa.png',
     links: [
       const ProjectLink(
         title: 'User App',
